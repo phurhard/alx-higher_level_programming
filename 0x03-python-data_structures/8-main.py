7b5f103f135cd3b89ee999c4634418f4b7ae5598
phurhard <phurhardeen@gmail.com>
2023-01-09 05:20:36 +0100

    Argh!!!

--- 0x03-python-data_structures/8-main.py
+++ 0x03-python-data_structures/8-main.py
@@ -2,8 +2,7 @@
 multiple_returns = __import__('8-multiple_returns').multiple_returns
 
 sentence = "At school, I learnt C!"
-sen = "Python is still way cooler than C, although i don't know if python\
-is simple or C has taught me how to think like a programmer" 
+sen = "" 
 length, first = multiple_returns(sentence)
 lengthe, firste = multiple_returns(sen)
 print("Length: {:d} - First character: {}".format(length, first))
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        return None
+        length = len(sentence)
+        first_char = None
+        return (length, first_char)
     else:
         length = len(sentence)
         first_char = sentence[0]
