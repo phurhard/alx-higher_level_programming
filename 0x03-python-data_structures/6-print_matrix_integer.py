95b7d10cc96f539616ed03326ba002c264dfb155
phurhard <phurhardeen@gmail.com>
2023-01-09 05:37:49 +0100

    Checks_are_failinh

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
+    if len(matrix) == 0:
         print()
     for element in range(len(matrix)):
         for item in range(len(matrix[element])):
