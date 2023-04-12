79c039abca2aff4269b3e8a0ddee4691fa810b8c
phurhard <phurhardeen@gmail.com>
2023-04-13 00:25:01 +0100

    square redone

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const multiplier = process.argv[2];
-let square = '';
+let str = '';
 if (!parseInt(multiplier)) {
   console.log('Missing size');
 } else {
-  for (let i = 1; i < parseInt(multiplier); i++) {
+  for (let i = 1; i <= parseInt(multiplier); i++) {
     for (let j = 0; j < parseInt(multiplier); j++) {
-      square += 'X';
+      str += 'X';
     }
-    square += '\n';
+    if (i !== parseInt(multiplier)) {
+      str += '\n';
+    }
   }
-  console.log(square);
+  console.log(str);
 }
