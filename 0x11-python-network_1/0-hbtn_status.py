ee2ae9946b260b382ae353595c5b3bc59c963671
phurhard <phurhardeen@gmail.com>
2023-05-03 00:22:26 +0100

    sre

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-'''This module is not executed when imported,
+"""This module is not executed when imported,
 and it prints the status of the webpage
-https://alx-intranet.hbtn.io/status'''
+https://alx-intranet.hbtn.io/status"""
 import urllib.request as url
-'''This python file displays the status of the webpage accessed using urllib'''
+
+"""This python file displays the status of the webpage accessed using urllib"""
+
 if __name__ == "__main__":
-    '''Starting out, making sure the module is not executed ehen imported'''
+    """Starting out, making sure the module is not executed when imported"""
+
     with url.urlopen("https://alx-intranet.hbtn.io/status") as req:
         status = req.read()
         print('Body response:')
-        print(f"\t - type: {type(status)}")
-        print(f"\t - content: {status}")
-        print(f"\t - utf content: {status.decode('utf-8')}")
+        print(f"\t- type: {type(status)}")
+        print(f"\t- content: {status}")
+        print(f"\t- utf content: {status.decode('utf-8')}")
+
