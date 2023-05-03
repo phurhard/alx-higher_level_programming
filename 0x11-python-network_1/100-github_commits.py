d6336fd5dcf2660649dcb0e83f6727867024b9dd
phurhard <phurhardeen@gmail.com>
2023-05-03 07:31:02 +0100

    used get()

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-''' Python script that takes a Github credentials and print the first 10 commits from newest to oldest
+''' Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+ * You must use Basic Authentication with a personal access
+ token as password to access to your information
+ (only read:user permission is needed)
+ * The first argument will be your username
+ * The second argument will be your password (in your case, a personal access token as password)
  * You must use the package requests and sys'''
 import requests
 import sys
@@ -8,12 +14,11 @@ if __name__ == "__main__":
     try:
         repoName = sys.argv[1]
         ownerName = sys.argv[2]
-        url = f"https://api.github.com/repos/{ownerName}/{repoName}/commits?author=rails"
-        req = requests.get(url)
+        req = requests.get(f"https://api.github.com/repos/{ownerName}/{repoName}/commits?author=rails")
         jsonFile = req.json()
         for commit in jsonFile[:10]:
-            sha = commit['sha']
-            authorName = commit['commit']['author']['name']
+            sha = commit.get('sha', 'unknown')
+            authorName = commit.get('commit', {}).get('author', {}).get('name', 'unknown')
             print(f'{sha}: {authorName}')
     except Exception as e:
         print(e)
